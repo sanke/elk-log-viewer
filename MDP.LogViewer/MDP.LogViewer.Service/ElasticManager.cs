1ddb5e39e7d33f9a3f5b7adff537dfe5cc2f6c36
Justinas <justinas.k@midpoint.lt>
2023-12-19 10:38:43 +0200

    Added button for clearing elastic stream

---
+++ MDP.LogViewer/MDP.LogViewer.Service/ElasticManager.cs
@@ -0,0 +1,23 @@
+﻿using Elastic.Clients.Elasticsearch;
+using Elastic.Clients.Elasticsearch.IndexManagement;
+using Elastic.Transport;
+
+namespace MDP.LogViewer.Service;
+
+public class ElasticManager
+{
+    private readonly ElasticsearchClient _client;
+
+    public ElasticManager()
+    {
+        var settings = new ElasticsearchClientSettings(new Uri("http://localhost:9200"))
+            .Authentication(new BasicAuthentication("elastic", "changeme"));
+
+        _client = new ElasticsearchClient(settings);
+    }
+
+    public async Task ClearAsync()
+    {
+        await _client.Indices.DeleteDataStreamAsync(new DeleteDataStreamRequest("logs-generic-default"));
+    }
+}
--- MDP.LogViewer/mdp-logviewer-client/src/app/app.component.ts
+++ MDP.LogViewer/mdp-logviewer-client/src/app/app.component.ts
@@ -6,20 +6,31 @@ import {CardModule} from "primeng/card";
 import {EditorModule} from "primeng/editor";
 import {MessageModule} from "primeng/message";
 import {MessagesModule} from "primeng/messages";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FileUploadModule, CardModule, EditorModule, MessageModule, MessagesModule ],
+  imports: [CommonModule, RouterOutlet, FileUploadModule, CardModule, EditorModule, MessageModule, MessagesModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   uploadedFiles: any[] = [];
 
+  constructor(private http: HttpClient) {
+  }
+
   onUpload($event: FileUploadEvent) {
     for (let file of $event.files) {
       this.uploadedFiles.push(file);
     }
+  }
+
+  clearExisting() {
+    console.log('clearing existing');
+    this.http.delete('/api/v1/clear').subscribe((res) => {
+      console.log('clearing existing done');
+    });
   }
 }
